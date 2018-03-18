87f9b70a4dc45d9d2d00adeba5f7226daff087ae
Tony Ambrus <azekeal@live.com>
2018-03-18 10:06:53 -0700

    Group connections

--- src/Agent/Agent.cs
+++ src/Agent/Agent.cs
@@ -23,11 +23,14 @@ namespace Agent
         public string Identifier { get; private set; }
         public string EndpointData { get; private set; }
 
+        private dynamic ActiveDispatcher => activeDispatcher != null ? DispatcherConnections[activeDispatcher] : null;
+
         private HubConnection coordinator;
         private IHubContext dispatcherHubContext;
         private IDisposable host;
-        private string activeDispatcherName;
-        private dynamic ActiveDispatcher;
+        private List<string> dispatcherQueue = new List<string>();
+        private string activeDispatcher;
+        private object lockObj = new object();
 
         public Agent()
         {
@@ -52,20 +55,8 @@ namespace Agent
             dispatcherHubContext = GlobalHost.ConnectionManager.GetHubContext<DispatcherHub>();
 
             DispatcherConnections = new DispatcherConnectionHandler(dispatcherHubContext);
-            DispatcherConnections.EndpointAdded += (name, connectionId, info) =>
-            {
-                if (activeDispatcherName == null)
-                {
-                    // TODO: groups
-                    activeDispatcherName = name;
-                    ActiveDispatcher = DispatcherConnections[name].FirstOrDefault();
-                }
-
-                foreach (var dispatcher in DispatcherConnections[name])
-                {
-                    dispatcher.SetAgentIdentifier(Identifier);
-                }
-            };
+            DispatcherConnections.EndpointAdded += AddDispatcher;
+            DispatcherConnections.EndpointRemoved += RemoveDispatcher;
 
             host = WebApp.Start(HostUrl);
             Console.WriteLine("Server running on {0}", HostUrl);
@@ -76,6 +67,54 @@ namespace Agent
             coordinator = new HubConnection($"http://localhost:{Constants.Ports.CoordinatorHost}/signalr", Identifier, EndpointData, "AgentHub");
             coordinator.Start();
         }
+
+        public void AddDispatcher(string name, string connectionInfo, EndpointConnectionInfo info)
+        {
+            lock (lockObj)
+            {
+                dispatcherQueue.Add(name);
+
+                if (activeDispatcher == null)
+                {
+                    ActivateNextDispatcher();
+                }
+            }
+        }
+
+        public void RemoveDispatcher(string name)
+        {
+            lock (lockObj)
+            {
+                dispatcherQueue.Remove(name);
+
+                if (activeDispatcher == name)
+                {
+                    ActivateNextDispatcher();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Currently we only support doing work for a single dispatcher at a time. 
+        /// Future work is needed to allow 2+ (configurable) dispatchers with different
+        /// runAs to subst
+        /// </summary>
+        private void ActivateNextDispatcher()
+        {
+            lock (lockObj)
+            {
+                if (dispatcherQueue.Count > 0)
+                {
+                    activeDispatcher = dispatcherQueue[0];
+                    dispatcherQueue.RemoveAt(0);
+                }
+                else
+                {
+                    activeDispatcher = null;
+                }
+            }
+        }
+
 
         internal TaskResult[] StartTasks(IEnumerable<TaskItem> tasks)
         {
--- src/Agent/DispatcherConnectionHandler.cs
+++ src/Agent/DispatcherConnectionHandler.cs
@@ -1,28 +1,14 @@
 ﻿using Common;
 using Microsoft.AspNet.SignalR;
-using System.Collections.Generic;
 
 namespace Agent
 {
     public class DispatcherConnectionHandler : ClientConnectionHandler
     {
-        private IHubContext dispatcherHubContext;
-
-        public DispatcherConnectionHandler(IHubContext dispatcherHubContext)
+        public DispatcherConnectionHandler(IHubContext dispatcherHubContext) : base(dispatcherHubContext)
         {
-            this.dispatcherHubContext = dispatcherHubContext;
         }
 
-        public IEnumerable<dynamic> this[string name]
-        {
-            get
-            {
-                foreach (var connectionId in ConnectionIds[name])
-                {
-                    // TODO: add all connection to the same user group
-                    yield return dispatcherHubContext.Clients.Client(connectionId);
-                }
-            }
-        }
+        public dynamic this[string name] => Connections.Group(name);
     }
 }
